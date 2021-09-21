fc5cc99f6165284ecfc5aef01e8eed176fe2e470
Lucas-M-R <l.morille-roy@codeur.online>
2021-09-21 20:19:27 +0200

    we're getting to it

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
+<?php header('Access-Control-Allow-Origin: *');  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Music-Band</title>
+   
 </head>
+
 <body>
     
+<h3>fetch get text</h3>
+<h3>fetch get json</h3>
+    <p style="color:red">
+         Click the button and see the results in the Console.
+    </p>
+
+    <button onclick="doGetJSON()">doGetJSON</button>
+    <div id="genreList"></div>
+    <div id="chartList"></div>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,100 @@
+// var myInit = { method: 'GET',
+//                mode: 'no-cors',
+//                cache: 'default' };
+
+
+
+// let deezer = fetch("https://ex1.o7planning.com/_testdatas_/simple-text-data.txt", myInit)
+// .then(response => response.json())
+// .then(response => console.log(JSON.stringify(response)))
+// .catch(error => console.log("Erreur : blaaah :" + error));
+
+
+
+// A URL returns JSON data.
+// var url = "https://api.deezer.com/genre/2";
+
+
+// function doGetJSON()  {
+
+//   // Call fetch(url) with default options.
+//   // It returns a Promise object:
+//   var aPromise = fetch(url);
+
+//   // Work with Promise object:
+//   aPromise
+//     .then(function(response) {
+//         console.log("OK! Server returns a response object:");
+//         console.log(response);
+
+//         if(!response.ok) {
+//            throw new Error("HTTP error, status = " + response.status);
+//         } 
+//         // Get JSON Promise from response object:
+//         var myJSON_promise = response.json();
+
+//         // Returns a Promise object.
+//         return myJSON_promise;
+//     })
+//     .then(function(myJSON) {
+//         console.log("OK! JSON:");
+//         console.log(myJSON);
+//     })
+//     .catch(function(error)  {
+//         console.log("Noooooo! Something error:");
+//         console.log(error);
+//     });
+
+// }
+
+let genresHtml = document.getElementById('genreList');
+
+var myOptions = { method: 'GET',
+               mode: 'cors',
+               cache: 'default' };
+const deezerGenre = fetch("https://api.deezer.com/genre", myOptions);
+deezerGenre
+.then(async(responseData)=>{
+// console.log(responseData);
+const response = await responseData.json();
+// console.log(response);
+try{
+    for (i=10; i>0; i--){
+const genreName = "<span>"+response.data[i].name+"</span>";
+const genrePicture = '<img src="'+response.data[i].picture+'">'; 
+let genreItem = document.createElement("div");
+genresHtml.appendChild(genreItem);
+genreItem.innerHTML = genrePicture+genreName;
+// document.getElementById('genreName').innerHTML += '<img src="'+genrePicture+'"> '+genreName;
+}}
+catch (err) {console.log(err);}
+})
+.catch((err) =>{ console.log(err)});
+
+
+
+
+
+
+let chartHtml = document.getElementById('chartList')
+
+const deezerTop = fetch("https://api.deezer.com/chart", myOptions);
+deezerTop
+.then(async(responseData)=>{
+// console.log(responseData);
+const response = await responseData.json();
+console.log(response);
+try{
+    for (i=0; i<=10; i++){
+const chartName = "<span>"+response.artists.data[i].name+"</span>";
+const chartPicture = '<img src="'+response.artists.data[i].picture+'">'; 
+let chartItem = document.createElement("div");
+chartHtml.appendChild(chartItem);
+chartItem.innerHTML = chartPicture+chartName;
+// document.getElementById('genreName').innerHTML += '<img src="'+genrePicture+'"> '+genreName;
+}}
+catch (err) {console.log(err);}
+})
+.catch((err) =>{ console.log(err)});
+
+
