8ad86a80723f4736b8fd6825456c296d60f48d76
BU33R <t.laubepin@codeur.online>
2021-09-27 13:57:10 +0200

    Voila

--- index.php
+++ index.php
@@ -19,19 +19,20 @@
     <nav>
         <div class="menuburger"><img src="SVG/MENU.svg"></div>
         <div class="musicband"> <b> Music Band </b> </br> La musique au bout des doigts </div>
-        <div class="searchbar"> Recherchez, écoutez... <div class="casque"><img class="displayed" src="SVG/Vector.svg">
-            </div>
+        <div class="searchbar"> Recherchez, écoutez... <div class="casque"><img class="displayed" src="SVG/Vector.svg"></div>
             <div class="avatar"> </div>
             <div class="user"> <b>Alain</b> </div>
-
     </nav>
     <header>
         <div>
             <h1><span>Music</span> Band</h1>
-            <div class="textheader"> <p>Votre musique sans pub, partout. Seulement 9,99€/mois ensuite. </br> Soumis à conditions. Des podcasts
-                exclusifs. </br> Des playlists sur-mesure. Faites des découvertes. </br> Zappez à volonté. Plus de 50
-                milliards de
-                chansons. </br> Annulation tout de suite! </p> 
+            <div class="textheader">
+                <p>Votre musique sans pub, partout. Seulement 9,99€/mois ensuite. </br> Soumis à conditions. Des
+                    podcasts
+                    exclusifs. </br> Des playlists sur-mesure. Faites des découvertes. </br> Zappez à volonté. Plus de
+                    50
+                    milliards de
+                    chansons. </br> Annulation tout de suite! </p>
             </div>
             <button id="start">commencer l'écoute</button>
             <button id="sub">s'abonner</button>
